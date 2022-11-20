d0f1cf094bf7ed4820b361b65c6cef12da607313
Tăng Quốc Thái - ITC - AI - Associate Lead MLE <thai.tang@mservice.com.vn>
2022-11-20 22:21:41 +0700

    Add GCS as the storage

--- voh/fetch_hls_stream.py
+++ voh/fetch_hls_stream.py
@@ -6,6 +6,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 import click
 import m3u8
+from google.cloud import storage
+from google.oauth2 import service_account
 from requests import get
 
 # Set representing chunks that we have already downloaded
@@ -16,6 +18,10 @@ dlpool = ThreadPoolExecutor(max_workers=4)
 
 # Logger
 logger = logging.getLogger("fetch_hls_stream")
+
+# GCS credentials
+credentials = service_account.Credentials.from_service_account_file(
+    'service-account.json')
 
 
 def setuplog(verbose):
@@ -29,18 +35,38 @@ def setuplog(verbose):
         logger.setLevel(logging.INFO)
 
 
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The contents to upload to the file
+    # contents = "these are my contents"
+
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client(credentials=credentials)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_string(contents)
+
+
 def download_file(uri, outputdir, filename):
     """Download a ts video and save on the outputdir as the following file:
     outputdir/date_filename"""
     try:
-        date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        date = datetime.datetime.now().strftime("%Y/%m/%d/%H_%M_%S")
         fpath = os.path.join(outputdir, date + "_" + filename)
-        with open(fpath, "wb") as file:
-            logger.info("DOWNLOADING FILE: " + uri)
-            response = get(uri)
-            file.write(response.content)
 
-            logger.debug("FINISHED WRITING " + uri + " TO FILE: " + fpath)
+        logger.info("DOWNLOADING FILE: " + uri)
+        response = get(uri)
+        upload_blob_from_memory(bucket_name="voh-project",
+                                contents=response.content,
+                                destination_blob_name=fpath)
+
+        logger.debug("FINISHED WRITING " + uri + " TO GCS: " + fpath)
     except Exception as ex:
         logger.error(ex)
 
