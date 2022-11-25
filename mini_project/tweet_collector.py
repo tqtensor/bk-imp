c0bbc31dbe37e3268d768c95213e56889e9b6f6e
Tang Quoc Thai <victortang212@gmail.com>
2022-11-25 15:26:35 +0000

    Ingest twitter stream to elasticsearch

--- mini_project/tweet_collector.py
+++ mini_project/tweet_collector.py
@@ -1,11 +1,14 @@
 import json
 import os
 
+from elasticsearch import Elasticsearch
 import requests
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("BEARER_TOKEN")
+
+es = Elasticsearch(hosts="http://localhost:9200")
 
 
 def bearer_oauth(r):
@@ -50,7 +53,7 @@ def set_rules(delete):
     sample_rules = [
         {
             "value":
-            "(context:46.1557697333571112960) (place_country:US OR place_country:MX OR place_country:CA)"
+                "(context:46.1557697333571112960) (place_country:US OR place_country:CA)"
         },
     ]
     payload = {"add": sample_rules}
@@ -78,14 +81,23 @@ def get_stream(set):
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
+            # Ingest to ElasticSearch
+            es_response = es.index(index="twitter",
+                                   document={
+                                       "id": json_response["data"]["id"],
+                                       "text": json_response["data"]["text"]
+                                   })
+            if es_response['result'] != "created":
+                raise RuntimeError("ElasticSearch index error: {}".format(
+                    es_response['result']))
 
 
 def main():
     rules = get_rules()
     delete = delete_all_rules(rules)
     set = set_rules(delete)
-    get_stream(set)
+    while True:
+        get_stream(set)
 
 
 if __name__ == "__main__":
