daecaf0db35cdaf8ba29a0793e9d7997a8b443d5
tqtensor <victortang212@gmail.com>
2023-05-06 15:18:10 +0000

    feat: add word2vec and encoded category features

--- get_amazon_reviews.sh
+++ get_amazon_reviews.sh
@@ -3,29 +3,32 @@
 # Set the directory where the JSON files should be stored
 DATA_DIR="bigdata-mini-project/data/"
 
-# Check if the Digital_Music.json file exists in the data directory
-if [ ! -f "${DATA_DIR}Digital_Music.json" ]; then
+# Prompt the user for the dataset name
+read -p "Enter the name of the Amazon review dataset to download (e.g., Digital_Music): " DATASET_NAME
+
+# Check if the JSON file for the specified dataset exists in the data directory
+if [ ! -f "${DATA_DIR}${DATASET_NAME}.json" ]; then
 
   # If the file does not exist, download it from the first link
-  echo "Downloading Digital_Music.json file..."
-  wget --no-check-certificate -P "${DATA_DIR}" "https://jmcauley.ucsd.edu/data/amazon_v2/categoryFiles/Digital_Music.json.gz"
+  echo "Downloading ${DATASET_NAME}.json file..."
+  wget --no-check-certificate -P "${DATA_DIR}" "https://jmcauley.ucsd.edu/data/amazon_v2/categoryFiles/${DATASET_NAME}.json.gz"
 
   # Extract the downloaded file
-  echo "Extracting Digital_Music.json file..."
-  gunzip "${DATA_DIR}Digital_Music.json.gz"
+  echo "Extracting ${DATASET_NAME}.json file..."
+  gunzip "${DATA_DIR}${DATASET_NAME}.json.gz"
 
 fi
 
-# Check if the meta_Digital_Music.json file exists in the data directory
-if [ ! -f "${DATA_DIR}meta_Digital_Music.json" ]; then
+# Check if the meta JSON file for the specified dataset exists in the data directory
+if [ ! -f "${DATA_DIR}meta_${DATASET_NAME}.json" ]; then
 
   # If the file does not exist, download it from the second link
-  echo "Downloading meta_Digital_Music.json file..."
-  wget --no-check-certificate -P "${DATA_DIR}" "https://jmcauley.ucsd.edu/data/amazon_v2/metaFiles2/meta_Digital_Music.json.gz"
+  echo "Downloading meta_${DATASET_NAME}.json file..."
+  wget --no-check-certificate -P "${DATA_DIR}" "https://jmcauley.ucsd.edu/data/amazon_v2/metaFiles2/meta_${DATASET_NAME}.json.gz"
 
   # Extract the downloaded file
-  echo "Extracting meta_Digital_Music.json file..."
-  gunzip "${DATA_DIR}meta_Digital_Music.json.gz"
+  echo "Extracting meta_${DATASET_NAME}.json file..."
+  gunzip "${DATA_DIR}meta_${DATASET_NAME}.json.gz"
 
 fi
 
