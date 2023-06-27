c8c34f04751d27155ff7c121e845b33037d2ad48
Tang Quoc Thai <victortang212@gmail.com>
2023-06-27 07:22:15 +0700

    feat: decoded locationId (#7)
    * chore: clean files
    
    * feat: add download nyc data script
    
    * feat: update download raw data script
    
    ---------
    
    Co-authored-by: tangquocthai <tangquocthai@tqthai-mba.lan>
    Co-authored-by: thai.tang <thai.tang@mservice.com.vn>

---
+++ ml/nyc-dataset/download_raw_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Ask the user to choose a dataset type
+read -p "Enter the dataset type (yellow, green, fhv, fhvhv): " dataset_type
+
+# Ask the user to choose a year or select "all years"
+read -p "Enter the year for trip data download (e.g., 2021) or type 'all' for all years: " year
+
+# Construct the dataset filter based on the chosen dataset type
+dataset_filter=""
+if [ "$dataset_type" != "all" ]; then
+    dataset_filter="$dataset_type"
+fi
+
+# Construct the URL filter based on the chosen year
+year_filter=""
+if [ "$year" != "all" ]; then
+    year_filter="$year"
+fi
+
+# Download the trip data using wget and apply the filters
+cd ml/nyc-dataset
+wget -i <(grep -i "$dataset_filter" raw_data_urls | grep -i "$year_filter") -P data/ -w 2
